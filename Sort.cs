bd8a3556f697e09cb98610a7c2393e0028131dc1
kemiterlane <kemiterlane@hotmail.com>
2016-03-15 20:06:49 +0800

    Added files via upload

--- Sort.cs
+++ Sort.cs
@@ -1,5 +1,14 @@
 ﻿using System;
+/// <summary>
+/// 排序方法集
+/// </summary>
 public class Sort {
+    /// <summary>
+    /// 插入排序
+    /// </summary>
+    /// <typeparam name="T">数组类型</typeparam>
+    /// <param name="n">待排序数组</param>
+    /// <returns>排序好的数组</returns>
     public static T[] Insertion_Sort<T>(T[] n) where T : IComparable {
         for(int i = 1;i < n.Length;i++) {
             T temp = n[i];
