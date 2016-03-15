4c7d0425c2de31a9e0b3537df8c119503eb3a3f0
kemiterlane <kemiterlane@hotmail.com>
2016-03-15 21:39:38 +0800

    Added files via upload

--- Sort.cs
+++ Sort.cs
@@ -8,8 +8,7 @@ public class Sort {
     /// </summary>
     /// <typeparam name="T">数组类型</typeparam>
     /// <param name="n">待排序数组</param>
-    /// <returns>排序好的数组</returns>
-    public static T[] Insertion_Sort<T>(T[] n) where T : IComparable {
+    public static void Insertion_Sort<T>(T[] n) where T : IComparable {
         for(int i = 1;i < n.Length;i++) {
             T temp = n[i];
             int j = i - 1;
@@ -19,6 +18,57 @@ public class Sort {
             }
             n[j + 1] = temp;
         }
-        return n;
+    }
+    /// <summary>
+    /// 分治法排序（归并排序）
+    /// </summary>
+    /// <typeparam name="T">数组类型</typeparam>
+    /// <param name="n">待排序的数组</param>
+    /// <param name="p">数组的起始下标</param>
+    /// <param name="r">数组的结束下标</param>
+    public static void Merge_Sort<T>(T[] n,int p,int r) where T : IComparable {
+        if(p < r) {
+            int q = (p + r) / 2;
+            Merge_Sort(n,p,q);
+            Merge_Sort(n,q + 1,r);
+            Merge(n,p,q,r);
+        }
+    }
+    /// <summary>
+    /// 合并数组
+    /// </summary>
+    /// <typeparam name="T">数组类型</typeparam>
+    /// <param name="n">待排序的数组</param>
+    /// <param name="p">数组1的起始下标</param>
+    /// <param name="q">数组1的结束下标</param>
+    /// <param name="r">数组2的结束下标</param>
+    private static void Merge<T>(T[] n,int p,int q,int r) where T : IComparable {
+        T[] Left = new T[q - p + 1];
+        T[] Right = new T[r - q];
+        for(int i = 0;i < Left.Length;i++) {
+            Left[i] = n[p + i];
+        }
+        for(int j = 0;j < Right.Length;j++) {
+            Right[j] = n[q + j + 1];
+        }
+        int a = 0, b = 0;
+        for(int k = p;k <= r;k++) {
+            if(a == Left.Length) {
+                n[k] = Right[b];
+                b = b + 1;
+            }
+            else if(b == Right.Length) {
+                n[k] = Left[a];
+                a = a + 1;
+            }
+            else if(Left[a].CompareTo(Right[b]) <= 0) {
+                n[k] = Left[a];
+                a = a + 1;
+            }
+            else {
+                n[k] = Right[b];
+                b = b + 1;
+            }
+        }
     }
 }
