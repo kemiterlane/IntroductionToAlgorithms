7cbdb539e8caacd580efdab4e540c76aafa49565
kemiterlane <kemiterlane@hotmail.com>
2016-03-15 19:57:36 +0800

    Added files via upload

---
+++ Sort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Sort {
+    public static T[] Insertion_Sort<T>(T[] n) where T : IComparable {
+        for(int i = 1;i < n.Length;i++) {
+            T temp = n[i];
+            int j = i - 1;
+            while(j >= 0 && temp.CompareTo(n[j]) <= 0) {
+                n[j + 1] = n[j];
+                j = j - 1;
+            }
+            n[j + 1] = temp;
+        }
+        return n;
+    }
+}
